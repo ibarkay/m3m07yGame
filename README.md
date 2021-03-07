fc58517e8299ab6c4918c2c7d86b72900e469bab
ibarkay <ibarkay@gmail.com>
2021-03-07 15:06:27 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#memory game
+
+pseudoscop
---
+++ index.js
@@ -0,0 +1,126 @@
+const cardContainer = document.querySelector('.card-container');
+const [smallBtn, mediumBtn, largeBtn] = document.querySelectorAll('button');
+let wins = 0;
+let initalCardsNumbers = 0;
+const btnList = [smallBtn, mediumBtn, largeBtn];
+
+smallBtn.addEventListener('click', () => {
+  initalCardsNumbers = 12;
+  safe(12);
+  for (const b of btnList) {
+    b.classList.add('disapear');
+  }
+});
+mediumBtn.addEventListener('click', () => {
+  initalCardsNumbers = 24;
+  safe(24);
+  for (const b of btnList) {
+    b.classList.add('disapear');
+  }
+});
+largeBtn.addEventListener('click', () => {
+  initalCardsNumbers = 36;
+  safe(36);
+  for (const b of btnList) {
+    b.classList.add('disapear');
+  }
+});
+
+console.log('🏊 ');
+const cardsArray = [];
+let currentData = 0;
+let currentCards = [];
+
+const createCards = (numOfCards) => {
+  for (let i = 0; i < numOfCards; i++) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    if (i % 2 === 0) {
+      currentData = i;
+    }
+    card.setAttribute('data', `${currentData}`);
+    card.innerHTML = `${currentData}`;
+    // ev on cards
+    card.addEventListener('click', (e) => {
+      const cardy = e.currentTarget;
+      console.log(currentCards);
+      if (currentCards.length < 2 && !currentCards.includes(cardy)) {
+        currentCards.push(cardy);
+        cardy.classList.remove('hide-card');
+        tick();
+        // console.log(cardy.getAttribute('data'));
+      }
+    });
+    cardsArray.push(card);
+  }
+};
+function clearCurrentList() {
+  setTimeout(() => {
+    currentCards = [];
+  }, 1000);
+}
+function checkForWin() {
+  if (wins === (initalCardsNumbers / 2)) {
+    alert('you won!');
+    location.reload();
+  }
+}
+async function waitFortime() {
+  await hideCards2();
+  await clearCurrentList();
+}
+function hideCards2() {
+  for (const card of currentCards) {
+    setTimeout(() => { card.classList.add('hide-card'); }, 1000);
+  }
+}
+function shuffle(array) {
+  let currentIndex = array.length; let temporaryValue; let
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+function appendCards() {
+  for (const card of cardsArray) {
+    cardContainer.append(card);
+  }
+}
+async function hideCards() {
+  for (const card of cardsArray) {
+    await card.classList.add('hide-card');
+  }
+}
+function tick() {
+  if (currentCards[1]) {
+    if (currentCards[0].getAttribute('data') === currentCards[1].getAttribute('data')) {
+      for (const card of currentCards) {
+        card.classList.add('disapear');
+      }
+      waitFortime();
+      wins++;
+      checkForWin();
+    } else {
+      waitFortime();
+    }
+  }
+}
+
+async function safe(numberOfCards) {
+  await createCards(numberOfCards);
+  await shuffle(cardsArray);
+  await hideCards();
+  await appendCards();
+}
+// safe(12); // call
